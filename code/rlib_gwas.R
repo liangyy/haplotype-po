b87d871bb622380127e3f992f45cdd9c209c3df4
Yanyu Liang <yanyul@uchicago.edu>
2020-04-09 00:13:42 -0500

    as of today

---
+++ code/rlib_gwas.R
@@ -0,0 +1,77 @@
+# X: matrix n x p
+# y: vector p x 1
+# run y ~ Xj
+run_gwas = function(X, y) {
+  a_y = sd(y)
+  b_x = apply(X, 2, sd)
+  y_tilde = y / a_y
+  # message(1)
+  x_tilde = sweep(X, 2, FUN = '/', b_x)
+  # message(2)
+  xtx = colSums(x_tilde ^ 2)
+  xbar = colMeans(x_tilde)
+  ybar = mean(y_tilde)
+  n = nrow(x_tilde)
+  # message(dim(x_tilde), '  ', length(y_tilde))
+  xty = colSums(sweep(x_tilde, 1, FUN = '*', y_tilde))
+  # message(3)
+  denom = xtx - n * xbar ^ 2
+  mu0 = 1 / denom * (xtx * ybar - xbar * xty)
+  bhat = 1 / denom * (-n * xbar * ybar + xty)
+  ypred = sweep(sweep(x_tilde, 2, FUN = '*', bhat), 2, FUN = '+', mu0)
+  # message(4)
+  sigma2 = 1 / (n - 2) * colSums((sweep(ypred, 1, FUN = '-', y_tilde)) ^ 2)
+  # message(5)
+  bhat_se = sqrt(sigma2 / (xtx - n * xbar ^ 2))
+  bhat = bhat * a_y / b_x
+  bhat_se = bhat_se * a_y / b_x
+  return(list(bhat = bhat, bhat_se = bhat_se))
+}
+
+# X: matrix n x p
+# y: matrix n x p
+# w: matrix n x p
+# run yj ~ Xj with weights wj
+run_gwas_pairwise = function(X, y, weights = NULL) {
+  x0 = matrix(1, nrow = nrow(X), ncol = ncol(X))
+  if(!is.null(weights)) {
+    X = sqrt(weights) * X
+    y = sqrt(weights) * y
+    x0 = sqrt(weights) * x0
+  }
+  x0 = sweep(x0, 2, FUN = '/', colSums(x0))
+  
+  a_y = apply(y, 2, sd)
+  b_x = apply(X, 2, sd)
+  y_tilde = sweep(y, 2, FUN = '/', a_y) 
+  x_tilde = sweep(X, 2, FUN = '/', b_x) 
+  xtx = colSums(x_tilde ^ 2)
+  xbar = colSums(x_tilde * x0) 
+  ybar = colSums(y_tilde * x0) 
+  n = colSums(x0 ^ 2)
+  xty = colSums(x_tilde * y_tilde)
+  denom = n * xtx - xbar ^ 2
+  mu0 = 1 / denom * (xtx * ybar - xbar * xty)
+  bhat = 1 / denom * (- xbar * ybar + xty * n)
+  ypred = sweep(x_tilde, 2, FUN = '*', bhat) + sweep(x0, 2, FUN = '*', mu0)
+  sigma2 = 1 / (nrow(x_tilde) - 2) * colSums((ypred - y_tilde) ^ 2)
+  bhat_se = sqrt(sigma2 * n / denom)
+  print(denom)
+  bhat = bhat * a_y / b_x
+  bhat_se = bhat_se * a_y / b_x
+  return(list(bhat = bhat, bhat_se = bhat_se))
+}
+
+# # X: matrix n x p
+# # y: matrix n x p
+# # w: matrix n x p
+# # run yj ~ Xj
+# run_gwas_pairwise = function(X, y, weights = NULL) {
+#   if(is.null(weights)) {
+#     return(.run_gwas_pairwise(X, y))
+#   } else {
+#     X = sqrt(weights) * X
+#     y = sqrt(weights) * y
+#     return(.run_gwas_pairwise(X, y))
+#   }
+# }
---
+++ code/rlib_simulation.R
@@ -0,0 +1,54 @@
+get_maf = function(p, maf_low, maf_high) {
+  return(runif(p, min = maf_low, max = maf_high))
+}
+sim_hap = function(n, p, maf) {
+  hlist = list()
+  for(i in 1:2) {
+    hlist[[i]] = matrix(rbinom(p * n, 1, rep(maf, n)), byrow = TRUE, nrow = n, ncol = p)
+  }
+  return(hlist)
+}
+transmit_haplo = function(father, mother) {
+  out = list(
+    matrix(NA, nrow = nrow(father[[1]]), ncol = ncol(father[[1]])),
+    matrix(NA, nrow = nrow(father[[2]]), ncol = ncol(father[[2]]))
+  )
+  for(i in 1 : nrow(out[[1]])) {
+    f_idx = sample(1:2, 1)
+    m_idx = sample(1:2, 1)
+    out[[1]][i, ] = father[[f_idx]][i, ]
+    out[[2]][i, ] = mother[[m_idx]][i, ]
+  }
+  return(out)
+}
+spike_and_slab = function(n, pi_, sigma2) {
+  b = rnorm(n, sd = sqrt(sigma2))
+  b[runif(n) <= pi_] = 0
+  return(b)
+}
+simulate_pheno = function(h, beta, h2, maf) {
+  n = nrow(h[[1]])
+  p = ncol(h[[1]])
+  k = ncol(beta)
+  var_snp = 2 * maf * (1 - maf)  # n x 1
+  var_genetics = t(beta ^ 2) %*% var_snp  # k x 1
+  var_e = (1 - h2) * var_genetics / h2  # k x 1
+  env_noise = matrix(rnorm(n * k, sd = sqrt(rep(var_e, n))), byrow = TRUE, ncol = k, nrow = n)  # n x k
+  pheno = h[[1]] %*% beta + h[[2]] %*% beta + env_noise
+  pheno
+}
+
+simulate_pheno_single_snp = function(h, beta, h2, maf) {
+  # h: matrix n x p
+  # beta: vector p x 1
+  # h2: vector (or scalar) p x 1
+  # maf: vector p x 1
+  n = nrow(h[[1]])
+  p = ncol(h[[1]])
+  var_snp = 2 * maf * (1 - maf)  # p x 1
+  var_genetics = beta ^ 2 * var_snp  # p x 1
+  var_e = (1 - h2) * var_genetics / h2  # p x 1
+  env_noise = matrix(rnorm(n * p, sd = sqrt(rep(var_e, n))), byrow = TRUE, ncol = p, nrow = n)  # n x k
+  pheno = h[[1]] %*% diag(beta) + h[[2]] %*% diag(beta) + env_noise
+  pheno
+}
