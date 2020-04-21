7ebe3be39fd9b4980acaaf33ab8b414a3bff9c1d
Yanyu Liang <yanyul@uchicago.edu>
2020-04-21 18:48:35 -0500

    added test chr16 scripts

---
+++ scripts/prs/test_inputs/test_chr16.sh
@@ -0,0 +1,23 @@
+DATADIR=test_inputs
+CHR=16
+TASKNAME=test_chr$CHR
+
+GWASYAML=$DATADIR/$TASKNAME.yaml
+SNPMAP=/vol/bmd/yanyul/UKB/haplotype_imputation/snp_map_for_neale_lab_gwas.with_sign.tsv.gz
+BGEN=/vol/bmd/meliao/data/haplotype/hap/ukb_hap_chr$CHR\_v2.bgen
+BGI=/vol/bmd/meliao/data/haplotype/hap_bgi/ukb_hap_chr$CHR\_v2.bgen.bgi
+SAMPLE=/vol/bmd/meliao/data/haplotype/link_files/ukb1952_v2_s487398.sample
+
+
+python naive_prs.py \
+  --gwas-yaml $GWASYAML \
+  --snp-map $SNPMAP \
+  --bgen $BGEN \
+  --bgi $BGI \
+  --sample $SAMPLE \
+  --pval-cutoffs 1e-5,0.001,0.1,1 \
+  --chromosome $CHR \
+  --output-hdf5 $DATADIR/$TASKNAME.h5 \
+  --snp-chunk-size 10 \
+  --cache-path-for-gwas-dict $DATADIR/$TASKNAME.pgz
+  
