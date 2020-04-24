c3297647bf1b92156a298486261577d4c393e23a
Yanyu Liang <yanyul@uchicago.edu>
2020-04-24 11:19:41 -0500

    some updates on ld clump and prs scripts

--- scripts/prs/README.md
+++ scripts/prs/README.md
@@ -19,5 +19,7 @@ bash run_prs.sh \
   16 \
   /vol/bmd/yanyul/GitHub/haplotype-po/scripts/prs/configs/chr16.yaml \
   5e-8,1e-7,1e-6,1e-5,1e-4,1e-3,0.01,0.05,0.1,0.5,1 \
-  $outdir
+  $outdir \
+  name-of-the-run \
+  <(cat ../../analysis_output/analysis_batch.gwas_neale_lab.txt |grep _group1_ | cut -f 1)
 ```
--- scripts/prs/run_prs.sh
+++ scripts/prs/run_prs.sh
@@ -3,6 +3,7 @@
 # ARGS3: pval cutoffs
 # ARGS4: outdir
 # ARGS5: name tag
+# ARGS6: gwas list
 
 CHR=$1
 OUTDIR=$4
@@ -10,6 +11,7 @@ NAMETAG=$5
 
 GWASYAML=$2
 PVALS=$3  # "5e-8,1e-7,1e-6,1e-5,1e-4,1e-3,0.01,0.05,0.1,0.5,1"
+GWASLIST=$6
 SNPMAP=/vol/bmd/yanyul/UKB/haplotype_imputation/snp_map_for_neale_lab_gwas.with_sign.tsv.gz
 BGEN=/vol/bmd/meliao/data/haplotype/hap/ukb_hap_chr$CHR\_v2.bgen
 BGI=/vol/bmd/meliao/data/haplotype/hap_bgi/ukb_hap_chr$CHR\_v2.bgen.bgi
@@ -28,5 +30,6 @@ python naive_prs.py \
   --output-hdf5 $OUTDIR/prs_naive.$NAMETAG.chr$CHR.h5 \
   --snp-chunk-size 100 \
   --cache-path-for-gwas-dict $OUTDIR/prs_naive.chr$CHR.pgz \
+  --gwas-list $GWASLIST \
   > $OUTDIR/prs_naive.$NAMETAG.chr$CHR.log 2>&1
 
