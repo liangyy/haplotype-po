e6dae6a87042da48edab26f2ecdee0a44c235373
Yanyu Liang <yanyul@uchicago.edu>
2020-04-28 18:33:47 -0500

    refactored logistic solver to handle singular case

--- scripts/logistic_gpu/logistic_gpu.py
+++ scripts/logistic_gpu/logistic_gpu.py
@@ -62,7 +62,22 @@ class BatchLogisticSolver:
         
         return RHS 
     
-    def batchIRLS(self, X, y, C, device=None, tol=1e-8, maxiter=100):
+    @staticmethod
+    def _divide_fill_zero(a, b):
+        o = torch.div(a, b)
+        o[b == 0] = 0
+        return o
+    
+    def _snp_level_convergence_checker(self, w_new, w_old):
+        '''
+        Return the max and all relative differences between w_new and w_old row-wise.
+        '''
+        nom = torch.pow(w_new - w_old, 2)
+        den = torch.pow(w_new, 2)
+        diff = self._divide_fill_zero(torch.sum(nom, axis=1), torch.sum(den, axis=1))
+        return diff.max(), diff
+    
+    def batchIRLS(self, X, y, C, device=None, tol=1e-8, maxiter=100, min_prob=1e-5, use_mask=False):
         '''
         Input
             X: Tensor(n, p)
@@ -71,6 +86,7 @@ class BatchLogisticSolver:
         Output:
             B_hat: Tensor(p, 1). Effect size estimates where mth entry is for logit(y) ~ X[:, p] + C.
             B_se: Tensor(p, 1). The corresponding SE's of the estimates.
+        min_prob: prob < min_prob ~~ 0 and prob > 1 - min_prob ~~ 1 to determine if a node is active or dead
         '''
         if tol > 1 or tol < 0:
             raise ValueError('The tol is relative tolerence so we expect tol in (0, 1).')
@@ -83,55 +99,78 @@ class BatchLogisticSolver:
         # initialize
         if device is None:
             Wcx = torch.zeros(p, k + 1)
-            XSX = torch.Tensor(k + 1, k + 1, pe)
+            XSX = torch.Tensor(k + 1, k + 1, p)
+            Mu = torch.Tensor(n, p)
+            S = torch.Tensor(n, p)
+            active_p = torch.zeros(p) == 0
+            SE = - torch.ones(k + 1, p)
             diffs = torch.ones(p) 
+            SKIP_MASK = torch.ones(p)
         else:
             Wcx = torch.zeros(p, k + 1).to(device)
             XSX = torch.Tensor(k + 1, k + 1, p).to(device)
+            Mu = torch.Tensor(n, p).to(device)
+            S = torch.Tensor(n, p).to(device)
+            active_p = torch.zeros(p).to(device) == 1
+            SE = - torch.ones(k + 1, p).to(device)
             diffs = torch.ones(p).to(device) 
+            SKIP_MASK = torch.ones(p).to(device) 
         
-        diff = tol + 1
+        max_diff = tol + 1
+        # diffs = torch.ones(p) 
         niter = 0
         
-        while diff > tol and niter < maxiter:
+        if use_mask is True:
+            SKIP_MASK = SKIP_MASK == 0
+        else:
+            SKIP_MASK = SKIP_MASK == 1
+        
+        while max_diff > tol and niter < maxiter:
+            
+            # generate mask
+            mask = SKIP_MASK | diffs > tol 
             
             # take a copy of current Wcx
             Wcx_old = Wcx.clone()
             
-            # compute mu, S, and X^T S X
-            Mu, S, XSX = self._calc_Mu_S_XSX(XSX, X, C, Wcx)
+            # compute mu, S, and XSX
+            Mu[:, mask], _, XSX[:, :, mask] = self._calc_Mu_S_XSX(XSX[:, :, mask], X[:, mask], C, Wcx[mask, :])
+            
+            # update active status
+            active_p[:, mask] = active_p[:, mask] & (Mu[:, mask] > min_prob) & (Mu[:, mask] < 1 - min_prob)
+            mask[:, mask] = mask[:, mask] & active_p[:, mask]
             
             # get RHS := X^T(S X W + Y - Mu)
-            RHS = self._calc_RHS(X, y, C, Wcx, Mu, XSX)
+            RHS = self._calc_RHS(X[:, mask], y, C, Wcx[mask, :], Mu[:, mask], XSX[:, :, mask]) 
 
             # solve XSX^{-1} x = RHS and update
-            Wcx, _ = torch.solve(
-                torch.einsum('kp->pk', RHS).view(p, k + 1, -1), 
-                torch.einsum('ijk->kij', XSX)
+            tmp_, _ = torch.solve(
+                torch.einsum('kp->pk', RHS).view(mask.sum(), k + 1, -1), 
+                torch.einsum('ijk->kij', XSX)[mask, :, :]
             )
-            Wcx = Wcx[:, :, 0]
+            Wcx[mask, :] = tmp_[:, :, 0]
             
-            diff = self._naive_convergence_checker(Wcx, Wcx_old)
+            max_diff, diffs[mask] = self._snp_level_convergence_checker(Wcx[mask, :], Wcx_old[mask, :])
             niter += 1
-        
-        # after iteration
-        _, _, XSX = self._calc_Mu_S_XSX(XSX, X, C, Wcx)
+            
+        _, _, XSX[:, :, active_p] = self._calc_Mu_S_XSX(XSX[:, :, active_p], X[:, active_p], C, Wcx[active_p, :])
         
         if device is None:
-            ONES = torch.eye(k + 1).view(k + 1, k + 1, -1).expand_as(XSX)  # Tensor(k + 1, k + 1)
+            ONES = torch.eye(k + 1).view(k + 1, k + 1, -1).expand_as(XSX[:, :, active_p])  # Tensor(k + 1, k + 1)
         else:
-            ONES = torch.eye(k + 1).to(device).view(k + 1, k + 1, -1).expand_as(XSX)  
-            
+            ONES = torch.eye(k + 1).to(device).view(k + 1, k + 1, -1).expand_as(XSX[:, :, active_p])   
+        
         VAR, _ = torch.solve(
             torch.einsum('ijk->kij', ONES), 
-            torch.einsum('ijk->kij', XSX)
+            torch.einsum('ijk->kij', XSX[:, :, active_p])
         )  # Tensor(k + 1, k + 1, p)
+        SE[:, active_p] = torch.sqrt(torch.diagonal(VAR, dim1=1, dim2=2)).T 
         
         # return B_hat, B_se
-        if diff > tol:
-            print(f'Warning: not converged! diff = {diff} > tol = {tol}')
+        # if max_diff > tol:
+        #     print(f'Warning: not converged! max_diff = {max_diff} > tol = {tol}')
         
-        return Wcx.T, torch.sqrt(torch.diagonal(VAR, dim1=1, dim2=2)).T 
+        return Wcx.T, SE, diffs < tol
         
     @staticmethod
     def _naive_convergence_checker(w_new, w_old):
@@ -155,97 +194,113 @@ class BatchLogisticSolver:
     def _logistic_func(u):
         return 1 / ( 1 + torch.exp(-u) )
 
-class BatchLogisticSolverWithMask(BatchLogisticSolver):
-    def __init__(self):
-        super().__init__()
-        self.name = 'BatchLogisticSolverWithMask'
-        self.stop_criteria = '''Iterate until x_i ~ y pair converges'''
-    
-    @staticmethod
-    def _divide_fill_zero(a, b):
-        o = torch.div(a, b)
-        o[b == 0] = 0
-        return o
-    
-    def _snp_level_convergence_checker(self, w_new, w_old):
-        '''
-        Return the max and all relative differences between w_new and w_old row-wise.
-        '''
-        nom = torch.pow(w_new - w_old, 2)
-        den = torch.pow(w_new, 2)
-        diff = self._divide_fill_zero(torch.sum(nom, axis=1), torch.sum(den, axis=1))
-        return diff.max(), diff
-    
-    def batchIRLS(self, X, y, C, device=None, tol=1e-8, maxiter=100):
-        '''
-        Input
-            X: Tensor(n, p)
-            y: Tensor(n, 1)
-            C: Tensor(n, k)
-        Output:
-            B_hat: Tensor(p, 1). Effect size estimates where mth entry is for logit(y) ~ X[:, p] + C.
-            B_se: Tensor(p, 1). The corresponding SE's of the estimates.
-        '''
-        if tol > 1 or tol < 0:
-            raise ValueError('The tol is relative tolerence so we expect tol in (0, 1).')
-            
-        # get dimensions
-        n = X.shape[0]
-        p = X.shape[1]
-        k = C.shape[1]
-        
-        # initialize
-        if device is None:
-            Wcx = torch.zeros(p, k + 1)
-            XSX = torch.Tensor(k + 1, k + 1, p)
-            diffs = torch.ones(p) 
-        else:
-            Wcx = torch.zeros(p, k + 1).to(device)
-            XSX = torch.Tensor(k + 1, k + 1, p).to(device)
-            diffs = torch.ones(p).to(device) 
-        
-        max_diff = tol + 1
-        # diffs = torch.ones(p) 
-        niter = 0
-        
-        while max_diff > tol and niter < maxiter:
-            
-            # generate mask
-            mask = diffs > tol
-            
-            # take a copy of current Wcx
-            Wcx_old = Wcx.clone()
-            
-            # compute mu, S, and XSX
-            Mu, S, XSX[:, :, mask] = self._calc_Mu_S_XSX(XSX[:, :, mask], X[:, mask], C, Wcx[mask, :])
-            
-            # get RHS := X^T(S X W + Y - Mu)
-            RHS = self._calc_RHS(X[:, mask], y, C, Wcx[mask, :], Mu, XSX[:, :, mask]) 
-
-            # solve XSX^{-1} x = RHS and update
-            tmp_, _ = torch.solve(
-                torch.einsum('kp->pk', RHS).view(mask.sum(), k + 1, -1), 
-                torch.einsum('ijk->kij', XSX)[mask, :, :]
-            )
-            Wcx[mask, :] = tmp_[:, :, 0]
-            
-            max_diff, diffs = self._snp_level_convergence_checker(Wcx, Wcx_old)
-            niter += 1
-            
-        _, _, XSX = self._calc_Mu_S_XSX(XSX, X, C, Wcx)
-        
-        if device is None:
-            ONES = torch.eye(k + 1).view(k + 1, k + 1, -1).expand_as(XSX)  # Tensor(k + 1, k + 1)
-        else:
-            ONES = torch.eye(k + 1).to(device).view(k + 1, k + 1, -1).expand_as(XSX)   
-        
-        VAR, _ = torch.solve(
-            torch.einsum('ijk->kij', ONES), 
-            torch.einsum('ijk->kij', XSX)
-        )  # Tensor(k + 1, k + 1, p)
-        
-        # return B_hat, B_se
-        if max_diff > tol:
-            print(f'Warning: not converged! max_diff = {max_diff} > tol = {tol}')
-        return Wcx.T, torch.sqrt(torch.diagonal(VAR, dim1=1, dim2=2)).T 
-        
+# class BatchLogisticSolverWithMask(BatchLogisticSolver):
+#     def __init__(self):
+#         super().__init__()
+#         self.name = 'BatchLogisticSolverWithMask'
+#         self.stop_criteria = '''Iterate until x_i ~ y pair converges'''
+# 
+#     @staticmethod
+#     def _divide_fill_zero(a, b):
+#         o = torch.div(a, b)
+#         o[b == 0] = 0
+#         return o
+# 
+#     def _snp_level_convergence_checker(self, w_new, w_old):
+#         '''
+#         Return the max and all relative differences between w_new and w_old row-wise.
+#         '''
+#         nom = torch.pow(w_new - w_old, 2)
+#         den = torch.pow(w_new, 2)
+#         diff = self._divide_fill_zero(torch.sum(nom, axis=1), torch.sum(den, axis=1))
+#         return diff.max(), diff
+# 
+#     def batchIRLS(self, X, y, C, device=None, tol=1e-8, maxiter=100, min_prob=1e-5):
+#         '''
+#         Input
+#             X: Tensor(n, p)
+#             y: Tensor(n, 1)
+#             C: Tensor(n, k)
+#         Output:
+#             B_hat: Tensor(p, 1). Effect size estimates where mth entry is for logit(y) ~ X[:, p] + C.
+#             B_se: Tensor(p, 1). The corresponding SE's of the estimates.
+#         min_prob: prob < min_prob ~~ 0 and prob > 1 - min_prob ~~ 1 to determine if a node is active or dead
+#         '''
+#         if tol > 1 or tol < 0:
+#             raise ValueError('The tol is relative tolerence so we expect tol in (0, 1).')
+# 
+#         # get dimensions
+#         n = X.shape[0]
+#         p = X.shape[1]
+#         k = C.shape[1]
+# 
+#         # initialize
+#         if device is None:
+#             Wcx = torch.zeros(p, k + 1)
+#             XSX = torch.Tensor(k + 1, k + 1, p)
+#             Mu = torch.Tensor(n, p)
+#             S = torch.Tensor(n, p)
+#             active_p = torch.zeros(p) == 0
+#             SE = - torch.ones(k + 1, p)
+#             diffs = torch.ones(p) 
+#         else:
+#             Wcx = torch.zeros(p, k + 1).to(device)
+#             XSX = torch.Tensor(k + 1, k + 1, p).to(device)
+#             Mu = torch.Tensor(n, p).to(device)
+#             S = torch.Tensor(n, p).to(device)
+#             active_p = torch.zeros(p).to(device) == 1
+#             SE = - torch.ones(k + 1, p).to(device)
+#             diffs = torch.ones(p).to(device) 
+# 
+#         max_diff = tol + 1
+#         # diffs = torch.ones(p) 
+#         niter = 0
+# 
+#         while max_diff > tol and niter < maxiter:
+# 
+#             # generate mask
+#             mask = diffs > tol 
+# 
+#             # take a copy of current Wcx
+#             Wcx_old = Wcx.clone()
+# 
+#             # compute mu, S, and XSX
+#             Mu[:, mask], _, XSX[:, :, mask] = self._calc_Mu_S_XSX(XSX[:, :, mask], X[:, mask], C, Wcx[mask, :])
+# 
+#             # update active status
+#             active_p[:, mask] = active_p[:, mask] & (Mu[:, mask] > min_prob) & (Mu[:, mask] < 1 - min_prob)
+#             mask[:, mask] = mask[:, mask] & active_p[:, mask]
+# 
+#             # get RHS := X^T(S X W + Y - Mu)
+#             RHS = self._calc_RHS(X[:, mask], y, C, Wcx[mask, :], Mu[:, mask], XSX[:, :, mask]) 
+# 
+#             # solve XSX^{-1} x = RHS and update
+#             tmp_, _ = torch.solve(
+#                 torch.einsum('kp->pk', RHS).view(mask.sum(), k + 1, -1), 
+#                 torch.einsum('ijk->kij', XSX)[mask, :, :]
+#             )
+#             Wcx[mask, :] = tmp_[:, :, 0]
+# 
+#             max_diff, diffs[mask] = self._snp_level_convergence_checker(Wcx[mask, :], Wcx_old[mask, :])
+#             niter += 1
+# 
+#         _, _, XSX[:, :, active_p] = self._calc_Mu_S_XSX(XSX[:, :, active_p], X[:, active_p], C, Wcx[active_p, :])
+# 
+#         if device is None:
+#             ONES = torch.eye(k + 1).view(k + 1, k + 1, -1).expand_as(XSX[:, :, active_p])  # Tensor(k + 1, k + 1)
+#         else:
+#             ONES = torch.eye(k + 1).to(device).view(k + 1, k + 1, -1).expand_as(XSX[:, :, active_p])   
+# 
+#         VAR, _ = torch.solve(
+#             torch.einsum('ijk->kij', ONES), 
+#             torch.einsum('ijk->kij', XSX[:, :, active_p])
+#         )  # Tensor(k + 1, k + 1, p)
+#         SE[:, active_p] = torch.sqrt(torch.diagonal(VAR, dim1=1, dim2=2)).T 
+# 
+#         # return B_hat, B_se
+#         # if max_diff > tol:
+#         #     print(f'Warning: not converged! max_diff = {max_diff} > tol = {tol}')
+# 
+# 
+#         return Wcx.T, SE, diffs < tol
+# 
