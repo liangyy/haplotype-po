335b72ed3045ad2c746f737f7a96ef10818c8447
Yanyu Liang <yanyu.liang@lambda0.cels.anl.gov>
2020-05-21 08:01:49 -0700

    added some runs for gw

--- scripts/haplotype_imputation/submission_scripts/submit.sh
+++ scripts/haplotype_imputation/submission_scripts/submit.sh
@@ -3,10 +3,10 @@
 # ARGS3: number of threads
 
 genodir=/lambda_stor/data/yanyul/UKB/ukb_hap_v2_to_hdf5_genomewide
-snpdir=full_traits_snp_list
-phenof=father_phenotype_no_hypertension.yaml
-phenom=mother_phenotype_no_hypertension.yaml 
-covar=covariates.yaml
+snpdir=/lambda_stor/data/yanyul/GitHub/haplotype-po/scripts/haplotype_imputation/submission_scripts/full_traits_snp_list
+phenof=/lambda_stor/data/yanyul/GitHub/haplotype-po/scripts/haplotype_imputation/submission_scripts/father_phenotype_no_hypertension.yaml
+phenom=/lambda_stor/data/yanyul/GitHub/haplotype-po/scripts/haplotype_imputation/submission_scripts/mother_phenotype_no_hypertension.yaml 
+covar=/lambda_stor/data/yanyul/GitHub/haplotype-po/scripts/haplotype_imputation/submission_scripts/covariates.yaml
 nthread=$3
 
 outdir=/lambda_stor/data/yanyul/UKB/haplotype_imputation/haplotype_impute_otf
@@ -14,5 +14,5 @@ outdir=/lambda_stor/data/yanyul/UKB/haplotype_imputation/haplotype_impute_otf
 
 for i in `seq $1 $2`
 do
-  screen -dmS impute-$i "bash -c "run.sh $genodir/ukb_hap_v2_to_hdf5.chr$i.h5 $snpdir/chr$i $phenof $phenom $covar $/lambda_stor/data/yanyul/UKB/haplotype_imputation/haplotype_impute_otf/chr$i $nthread
+  screen -dmS impute-$i bash -c "bash run.sh $genodir/ukb_hap_v2_to_hdf5.chr$i.h5 $snpdir/chr$i $phenof $phenom $covar $outdir/chr$i $nthread"
 done
