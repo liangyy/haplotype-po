f4c751efbf54ea76ff45368b81bffe00dfc6fd1e
Yanyu Liang <yanyul@uchicago.edu>
2020-05-27 15:41:56 -0500

    added submitting script for multi chr

---
+++ scripts/haplotype_imputation/submission_scripts/submit_multi_chr.sh
@@ -0,0 +1,18 @@
+# ARGS1: chromosomes (separated by ,)
+# ARGS2: genotype prefix pattern
+# ARGS3: prefix of other npy preloaded files
+# ARGS4: output prefix
+
+CHROMS=$1
+genotypePREFIX=$2
+preloadNPY=$3
+OUT=$4
+
+
+python impute_otf_multi_chr.py \
+  --genotype-prefix-pattern $genotypePREFIX.chr{chr_num} \
+  --chromosomes $CHROMS \
+  --npy-prefix $preloadNPY \
+  --output-prefix $OUT.chr \
+  --imputer-output $OUT.pkl.gz 
+  
