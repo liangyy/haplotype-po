b9e6c13e0c29c141a683227ad099e9ae0a9be3fe
Yanyu Liang <yanyu.liang@lambda2.cels.anl.gov>
2020-05-28 08:15:36 -0700

    have a runnable version of per chr h2

--- scripts/per_chromosome_heritability/annot_rsid.py
+++ scripts/per_chromosome_heritability/annot_rsid.py
@@ -13,10 +13,24 @@ parser.add_argument('--output', help='''
     Output
 ''')
 args = parser.parse_args()
+
+import logging, time, sys, os
+# configing util
+logging.basicConfig(
+    level = logging.INFO,
+    stream = sys.stderr,
+    format = '%(asctime)s  %(message)s',
+    datefmt = '%Y-%m-%d %I:%M:%S %p'
+)
+
 import pandas as pd
 
+
+logging.info('Loading GWAS')
 gwas = pd.read_csv(args.gwas, sep='\t', compression='gzip')
-var_meta = pd.read_csv(args.variant_meta, sep='\t', compression='gzip')
+logging.info('Loading meta information of variants')
+var_meta = pd.read_csv(args.variant_meta, sep='\t', compression='gzip', usecols=['variant', 'rsid', 'ref', 'alt'])
+logging.info('Merging')
 gwas = pd.merge(gwas, var_meta[['variant', 'rsid', 'ref', 'alt']], left_on='variant', right_on='variant', how='inner')
 gwas.rename(
     columns = {
@@ -25,6 +39,6 @@ gwas.rename(
     }, 
     inplace = True
 )
-
+logging.info('Output')
 gwas.to_csv(args.output, sep='\t', compression='gzip', index=False)
 
