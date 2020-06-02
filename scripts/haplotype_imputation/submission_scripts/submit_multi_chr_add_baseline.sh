8bcc78e49cb7d73b1c93a815122384ba671d7b8a
Yanyu Liang <yanyu.liang@lambda0.cels.anl.gov>
2020-06-02 16:18:43 -0700

    leftover ukb run script

--- scripts/haplotype_imputation/submission_scripts/submit_multi_chr_add_baseline.sh
+++ scripts/haplotype_imputation/submission_scripts/submit_multi_chr_add_baseline.sh
@@ -1,9 +1,12 @@
-inprefix=/lambda_stor/data/yanyul/UKB/haplotype_imputation/haplotype_impute_otf_multi_chr/results/no_hypertension
+inprefix=/lambda_stor/data/yanyul/UKB/haplotype_imputation/haplotype_impute_otf_multi_chr/results/no_hypertension.chr
 insuffix=.tsv.gz
-outprefix=/lambda_stor/data/yanyul/UKB/haplotype_imputation/haplotype_impute_otf_multi_chr/results/no_hypertension
+outprefix=/lambda_stor/data/yanyul/UKB/haplotype_imputation/haplotype_impute_otf_multi_chr/results/no_hypertension.chr
 outsuffix=.w_baseline.tsv.gz
+
+cd ../
 
 for i in `seq 1 22`
 do
   python add_baseline.py --input $inprefix$i$insuffix --output $outprefix$i$outsuffix
 done
+
