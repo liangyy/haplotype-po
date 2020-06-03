ad3a9a4c76ed0b9d3fe707f8acfc4db9823582a4
Yanyu Liang <yanyu.liang@lambda0.cels.anl.gov>
2020-06-02 18:36:56 -0700

    in the middle of pred expr

--- scripts/framingham_detour/calc_pred_expr/submit.sh
+++ scripts/framingham_detour/calc_pred_expr/submit.sh
@@ -1,8 +1,9 @@
 # ARGS1: configfile
+# ARGS2: jobname
 
 source ~/conda_init.sh
 source ~/snmk_init.sh
 conda activate metaxcan
 
-cmd="$SNMK --configfile $1 -s run.snmk -np"  
-
+cmd="$SNMK --configfile $1 -s run.snmk -p > $2.log 2>&1"  
+screen -dmS $2 bash -c "$cmd"
